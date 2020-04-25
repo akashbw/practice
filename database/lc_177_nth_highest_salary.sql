e697c42cf52b8efd96f3eb06c48ddee374714950
Akash Jain <akashjain0108@gmail.com>
2020-04-25 23:16:15 +0800

    solved leetcode database problem 176 (second hightest salary) and 177 (nth highest salary)

---
+++ database/lc_177_nth_highest_salary.sql
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/nth-highest-salary/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+RETURN (
+   SELECT (
+       SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+       LIMIT 1 OFFSET M
+   ) 
+);
+END
